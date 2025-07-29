287e4e53ed01ebeb3061167d127f7bd30d55fcaa
dangquang <quangtd29@fpt.com.vn>
2025-07-29 22:57:29 +0700

    page 3

---
+++ src/DemoPage3.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const DemoPage3: React.FC = () => {
+  return (
+    <div style={{ padding: 32 }}>
+      <h1>Demo Page 3</h1>
+      <p>This is a third demo page for your Vite + React project.</p>
+    </div>
+  );
+};
+
+export default DemoPage3;
--- src/Root.tsx
+++ src/Root.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import DemoPage1 from "./DemoPage1";
 import DemoPage2 from "./DemoPage2";
+import DemoPage3 from "./DemoPage3";
 import App from "./App";
 
 const Root: React.FC = () => (
@@ -14,12 +15,16 @@ const Root: React.FC = () => (
       <Link to="/demo1" style={{ marginRight: 16 }}>
         Demo Page 1
       </Link>
-      <Link to="/demo2">Demo Page 2</Link>
+      <Link to="/demo2" style={{ marginRight: 16 }}>
+        Demo Page 2
+      </Link>
+      <Link to="/demo3">Demo Page 3</Link>
     </nav>
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/demo1" element={<DemoPage1 />} />
       <Route path="/demo2" element={<DemoPage2 />} />
+      <Route path="/demo3" element={<DemoPage3 />} />
     </Routes>
   </Router>
 );
